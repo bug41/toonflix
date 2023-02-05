87e39a09ff0d2ace36aba42b9a7e4cc705e7ca33
bugkingus <bugkingus@gmail.com>
2023-02-06 08:40:09 +0900

    플러터 깃 업로드 테스트

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+//위젯으로 바꿔주기 위해 가장 기초적이고 쉬운 StatelessWidget 상속
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //어떤 family 스타일 쓰고싶은지 선언하기
+    //return CupertinoApp()
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello flutter!'),
+          ),
+          body: Center(
+            child: Text('Hello world!'),
+          )),
+    );
+  }
+}

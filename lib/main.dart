92367d3c65f3c7457941b9232850f5fd74e991c8
bugkingus <bugkingus@gmail.com>
2023-02-20 08:36:30 +0900

    포모도로 제작, flex

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,29 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
 //위젯으로 바꿔주기 위해 가장 기초적이고 쉬운 StatelessWidget 상속
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     //어떤 family 스타일 쓰고싶은지 선언하기
     //return CupertinoApp()
-    return const MaterialApp();
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    //초기화 할때 호출되는 메소드
-    super.initState();
-    print('initState!');
-  }
-
-  @override
-  void dispose() {
-    // 위젯이 스크린에서 제거됐을때 호출되는 메소드
-    super.dispose();
-    print('dispose!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build!');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
       ),
+      home: const HomeScreen(),
     );
   }
 }

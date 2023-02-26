535ea784b8648b5f4f306fd21b41a327f68abb0e
bug41 <bugkinguswit@gmail.com>
2023-02-26 13:58:04 +0900

    static 화 , StatefulWidget 으로 변경

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  static String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
 
-  final String today = "today";
+  static String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
 
     final url = Uri.parse('$baseUrl/$today');

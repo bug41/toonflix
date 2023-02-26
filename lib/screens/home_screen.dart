4ae8dc5947707b43f6c8b6df7a1abf3360f2d465
bugkingus <bugkingus@gmail.com>
2023-02-27 08:52:49 +0900

    ListView.separated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +25,26 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: FutureBuilder(future: webtoons, builder: (context, snapshot) {}),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 40),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }

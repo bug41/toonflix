1fcc62338666edcd0a731e3030429e009bef8ce5
bug41 <bugkinguswit@gmail.com>
2023-03-05 10:13:07 +0900

    다른 스크린 처럼 보이기, 별도 스크린 생성

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -13,21 +13,47 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        title: const Center(
-          child: Text(
-            "오늘의 웹툰",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          elevation: 2,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          title: Center(
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(1),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(thumb),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -19,8 +19,12 @@ class Webtoon extends StatelessWidget {
           context,
           MaterialPageRoute(
             //another class , 다른 스크린 처럼 보이게 해줌
-            builder: (context) =>
-                DetailScreen(title: title, thumb: thumb, id: id),
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+            fullscreenDialog: true, // 기존에 옆에서 뜨던 새창을 밑에서 뜨게하고 아이콘도 x 로 변경
           ),
         );
       },
@@ -39,7 +43,10 @@ class Webtoon extends StatelessWidget {
                 ),
               ],
             ),
-            child: Image.network(thumb),
+            child: Image.network(
+              thumb,
+              //headers: const {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",},
+            ),
           ),
           const SizedBox(
             height: 10,

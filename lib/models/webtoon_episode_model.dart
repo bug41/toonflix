3525642c5eba9beb197f39daad4ef8a69ccbe0b5
bug41 <bugkinguswit@gmail.com>
2023-03-05 10:48:05 +0900

    웹툰 상세페이지 api 추가 및 class 추가

---
+++ lib/models/webtoon_episode_model.dart
@@ -0,0 +1,11 @@
+class WebtoonEpisodeModel {
+  final String id, title, rating, date;
+
+  WebtoonEpisodeModel(this.id, this.title, this.rating, this.date);
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        rating = json['rating'],
+        date = json['date'];
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   static String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-
   static String today = "today";
 
   static Future<List<WebtoonModel>> getTodaysToons() async {
@@ -24,6 +25,33 @@ class ApiService {
       return webtoonInstances;
     }
 
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+        //WebtoonEpisodeModel.fromJson(episode);
+      }
+      return episodesInstances;
+    }
     throw Error();
   }
 }
